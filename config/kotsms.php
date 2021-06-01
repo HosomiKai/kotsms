eb75edd15c9177a1ac0102cb267456612fde94f9
hosomikai <showking111@gmail.com>
2021-06-01 20:19:45 +0800

    style(config): move config

--- config/kotsms.php
+++ config/kotsms.php
@@ -1,8 +1,9 @@
 <?php
 
-
 return [
     'username' => env('Kotsms_Username', ''),       //簡訊王帳號
     'password' => env('Kotsms_Password', ''),       //簡訊王密碼
     'ReturnURL' => env('Kotsms_ReturnURL', ''),     //發送簡訊是否成功的狀態回報網址, 若不宣告此參數時為不回報。
+
+    'load_demo_service' => false,
 ];
